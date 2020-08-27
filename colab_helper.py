c8942abad10038ed8b66daca7cf968fb8c4e1185
Abhiraj Tiwari <abhirajtiwari@gmail.com>
2020-08-27 19:24:55 +0530

    secured against wrong arguments

--- colab_helper.py
+++ colab_helper.py
@@ -2,6 +2,7 @@ import requests
 from colab_helper.secrets import bot_token, bot_chatID
 
 def tg_sendMessage(msg):
+    assert(isinstance(msg, str))
     send_url = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + msg
 
     response = requests.get(send_url)
@@ -9,4 +10,5 @@ def tg_sendMessage(msg):
     return response.json
 
 def notifyTrainFinish(name):
+    assert(isinstance(name, str))
     tg_sendMessage('Training finished for '+name)
